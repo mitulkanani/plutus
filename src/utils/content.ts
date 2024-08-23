8a6da58e092820ca69b4f3fdd3b726f05545fc44
Rishabh Bhargav <rbhargav207@gmail.com>
2024-08-23 00:06:06 -0400

    Update content.ts

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,114 +1,170 @@
 export const navbarData = [
-    {
-        id: 1,
-        title: "Picks",
-        link: "/picks"
-    },
-    {
-        id: 2,
-        title: "Market Mood",
-        link: "/marketmood"
-    },
-    {
-        id: 3,
-        title: "Portfolio",
-        link: "/portfolio"
-    },
-    {
-        id: 4,
-        title: "News",
-        link: "/news"
-    },
-    {
-        id: 5,
-        title: "Analysis",
-        link: "/analysis"
-    },
-]
+  {
+    id: 1,
+    title: "Picks",
+    link: "/picks",
+  },
+  {
+    id: 2,
+    title: "Market Mood",
+    link: "/marketmood",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    id: 4,
+    title: "News",
+    link: "/news",
+  },
+  {
+    id: 5,
+    title: "Analysis",
+    link: "/analysis",
+  },
+];
 
 export const DifferentEquityMarketsData = [
-    {
-        id: 1,
-        title: "Indian Market",
-        link: "/picks/equitymarkets/indian"
-    },
-    {
-        id: 2,
-        title: "American Market",
-        link: "/picks/equitymarkets/american"
-    },
-    {
-        id: 3,
-        title: "UK Market",
-        link: "/picks/equitymarkets/uk"
-    },
-    {
-        id: 4,
-        title: "Hongkong Market",
-        link: "/picks/equitymarkets/hongkong"
-    },
-    {
-        id: 5,
-        title: "Saudi Arabia Market",
-        link: "/picks/equitymarkets/saudiarebia"
-    },
-]
+  {
+    id: 1,
+    title: "Indian Market",
+    link: "/picks/equitymarkets/indian",
+  },
+  {
+    id: 2,
+    title: "American Market",
+    link: "/picks/equitymarkets/american",
+  },
+  {
+    id: 3,
+    title: "UK Market",
+    link: "/picks/equitymarkets/uk",
+  },
+  {
+    id: 4,
+    title: "Hongkong Market",
+    link: "/picks/equitymarkets/hongkong",
+  },
+  {
+    id: 5,
+    title: "Saudi Arabia Market",
+    link: "/picks/equitymarkets/saudiarebia",
+  },
+];
 
 export const DifferentMarketsData = [
-    {
-        id: 1,
-        title: "Indian",
-        link: "/picks/markets/indian",
-        port: "/india"
-    },
-    {
-        id: 2,
-        title: "American",
-        link: "/picks/markets/american",
-        port: "/us"
-    },
-    {
-        id: 3,
-        title: "UK",
-        link: "/picks/markets/uk",
-        port: "/uk"
-    },
-    {
-        id: 4,
-        title: "Hongkong",
-        link: "/picks/markets/hongkong",
-        port: "/hk"
-    },
-    {
-        id: 5,
-        title: "Saudi Arabia",
-        link: "/picks/markets/saudiarebia",
-        port: "/saudi"
-    },
-]
+  {
+    id: 1,
+    title: "Indian",
+    link: "/picks/markets/indian",
+    port: "/india",
+  },
+  {
+    id: 2,
+    title: "American",
+    link: "/picks/markets/american",
+    port: "/us",
+  },
+  {
+    id: 3,
+    title: "UK",
+    link: "/picks/markets/uk",
+    port: "/uk",
+  },
+  {
+    id: 4,
+    title: "Hongkong",
+    link: "/picks/markets/hongkong",
+    port: "/hk",
+  },
+  {
+    id: 5,
+    title: "Saudi Arabia",
+    link: "/picks/markets/saudiarebia",
+    port: "/saudi",
+  },
+];
+export const DifferentMarketsIntervalData = [
+  {
+    id: 1,
+    title: "15 minutes",
+    indianPort: "india/der/15",
+    usport: "/us/der/15",
+    ukport: "/uk/der/15",
+    hongkongport: "/hk/der/15",
+    saudiport: "/saudi/der/15",
+  },
+  {
+    id: 2,
+    title: "30 minutes",
+    indianPort: "/india/der/30",
+    usport: "/us/der/30",
+    ukport: "/uk/der/30",
+    hongkongport: "/hk/der/30",
+    saudiport: "/saudi/der/30",
+  },
+  {
+    id: 3,
+    title: "60 minutes",
+    indianPort: "/india/der/60",
+    usport: "/us/der/60",
+    ukport: "/uk/der/60",
+    hongkongport: "/hk/der/60",
+    saudiport: "/saudi/der/60",
+  },
+  {
+    id: 4,
+    title: "daily",
+    indianPort: "/india/der/daily",
+    usport: "/us/der/daily",
+    ukport: "/uk/der/daily",
+    hongkongport: "/hk/der/daily",
+    saudiport: "/saudi/der/daily",
+  },
+  {
+    id: 5,
+    title: "weekly",
+    indianPort: "/india/der/weekly",
+    usport: "/us/der/weekly",
+    ukport: "/uk/der/weekly",
+    hongkongport: "/hk/der/weekly",
+    saudiport: "/saudi/der/weekly",
+  },
+  {
+    id: 6,
+    title: "monthly",
+    indianPort: "/india/der/monthly",
+    usport: "/us/der/monthly",
+    ukport: "/uk/der/monthly",
+    hongkongport: "/hk/der/monthly",
+    saudiport: "/saudi/der/monthly",
+  },
+];
 
 export const CallOptionsData = [
-    {
-        id: 1,
-        title: "IDEAFIRTB.NS"
-    },
-    {
-        id: 2,
-        title: "DELTACORP.NS"
-    },
-    {
-        id: 2,
-        title: "DELTACORP.NS"
-    },
-    {
-        id: 2,
-        title: "DELTACORP.NS"
-    },
-    {
-        id: 2,
-        title: "DELTACORP.NS"
-    },
-]
+  {
+    id: 1,
+    title: "IDEAFIRTB.NS",
+  },
+  {
+    id: 2,
+    title: "DELTACORP.NS",
+  },
+  {
+    id: 2,
+    title: "DELTACORP.NS",
+  },
+  {
+    id: 2,
+    title: "DELTACORP.NS",
+  },
+  {
+    id: 2,
+    title: "DELTACORP.NS",
+  },
+];
 
 // export const PutOptionsData = [
 //     {
@@ -122,192 +178,189 @@ export const CallOptionsData = [
 // ]
 
 export const intervalsData = [
-    {
-        id: 1,
-        title: "15 minutes",
-        indianPort: "/india/15",
-        usport: "/us/15",
-        ukport: "/uk/15",
-        hongkongport: "/hk/15",
-        saudiport: "/saudi/15",
-    },
-    {
-        id: 2,
-        title: "30 minutes",
-        indianPort: "/india/30",
-        usport: "/us/30",
-        ukport: "/uk/30",
-        hongkongport: "/hk/30",
-        saudiport: "/saudi/30"
-    },
-    {
-        id: 3,
-        title: "60 minutes",
-        indianPort: "/india/60",
-        usport: "/us/60",
-        ukport: "/uk/60",
-        hongkongport: "/hk/60",
-        saudiport: "/saudi/60"
-    },
-    {
-        id: 4,
-        title: "Daily",
-        indianPort: "/india/daily",
-        usport: "/us/daily",
-        ukport: "/uk/daily",
-        hongkongport: "/hk/daily",
-        saudiport: "/saudi/daily"
-    },
-    {
-        id: 5,
-        title: "Weekly",
-        indianPort: "/india/weekly",
-        usport: "/us/weekly",
-        ukport: "/uk/weekly",
-        hongkongport: "/hk/weekly",
-        saudiport: "/saudi/weekly"
-    },
-    {
-        id: 6,
-        title: "Monthly",
-        indianPort: "/india/monthly",
-        usport: "/us/monthly",
-        ukport: "/uk/monthly",
-        hongkongport: "/uk/monthly",
-        saudiport: "/saudi/monthly"
-    },
-]
+  {
+    id: 1,
+    title: "15 minutes",
+    indianPort: "/india/15",
+    usport: "/us/15",
+    ukport: "/uk/15",
+    hongkongport: "/hk/15",
+    saudiport: "/saudi/15",
+  },
+  {
+    id: 2,
+    title: "30 minutes",
+    indianPort: "/india/30",
+    usport: "/us/30",
+    ukport: "/uk/30",
+    hongkongport: "/hk/30",
+    saudiport: "/saudi/30",
+  },
+  {
+    id: 3,
+    title: "60 minutes",
+    indianPort: "/india/60",
+    usport: "/us/60",
+    ukport: "/uk/60",
+    hongkongport: "/hk/60",
+    saudiport: "/saudi/60",
+  },
+  {
+    id: 4,
+    title: "daily",
+    indianPort: "/india/daily",
+    usport: "/us/daily",
+    ukport: "/uk/daily",
+    hongkongport: "/hk/daily",
+    saudiport: "/saudi/daily",
+  },
+  {
+    id: 5,
+    title: "weekly",
+    indianPort: "/india/weekly",
+    usport: "/us/weekly",
+    ukport: "/uk/weekly",
+    hongkongport: "/hk/weekly",
+    saudiport: "/saudi/weekly",
+  },
+  {
+    id: 6,
+    title: "monthly",
+    indianPort: "/india/monthly",
+    usport: "/us/monthly",
+    ukport: "/uk/monthly",
+    hongkongport: "/uk/monthly",
+    saudiport: "/saudi/monthly",
+  },
+];
 
 export const EquityMarketsData = [
-    {
-        id: 1,
-        title: "HINDUNILVR"
-    },
-    {
-        id: 2,
-        title: "HINDUNILVR"
-    },
-    {
-        id: 3,
-        title: "HINDUNILVR"
-    },
-    {
-        id: 4,
-        title: "HINDUNILVR"
-    },
-    {
-        id: 5,
-        title: "HINDUNILVR"
-    },
-]
+  {
+    id: 1,
+    title: "HINDUNILVR",
+  },
+  {
+    id: 2,
+    title: "HINDUNILVR",
+  },
+  {
+    id: 3,
+    title: "HINDUNILVR",
+  },
+  {
+    id: 4,
+    title: "HINDUNILVR",
+  },
+  {
+    id: 5,
+    title: "HINDUNILVR",
+  },
+];
 
 export const PortfolioSimulationData = [
-    {
-        id: 1,
-        title: "Value-at-Risk",
-        number: "-2.5%"
-    },
-    {
-        id: 2,
-        title: "Loss rate",
-        number: "0.8%"
-    },
-    {
-        id: 3,
-        title: "Specific Excess Returns",
-        number: "1.2%"
-    },
-    {
-        id: 4,
-        title: "Portfolio jensen's Alpha",
-        number: "0.5%"
-    },
-    {
-        id: 5,
-        title: "Returns",
-        number: "4.3%"
-    },
-]
+  {
+    id: 1,
+    title: "Value-at-Risk",
+    number: "-2.5%",
+  },
+  {
+    id: 2,
+    title: "Loss rate",
+    number: "0.8%",
+  },
+  {
+    id: 3,
+    title: "Specific Excess Returns",
+    number: "1.2%",
+  },
+  {
+    id: 4,
+    title: "Portfolio jensen's Alpha",
+    number: "0.5%",
+  },
+  {
+    id: 5,
+    title: "Returns",
+    number: "4.3%",
+  },
+];
 
 export const AnalysisViewRegion = [
-    {
-        id: 1,
-        image: "/svg/flag.svg",
-        title: "India",
-        port: "/analysis/india"
-    },
-    {
-        id: 2,
-        image: "/svg/flag.svg",
-        title: "US",
-        port: "/analysis/us"
-    },
-    {
-        id: 3,
-        image: "/svg/flag.svg",
-        title: "UK",
-        port: "/analysis/uk"
-    },
-    {
-        id: 4,
-        image: "/svg/flag.svg",
-        title: "Saudi",
-        port: "/analysis/saudi"
-    },
-    {
-        id: 5,
-        image: "/svg/flag.svg",
-        title: "Hong Kong",
-        port: "/analysis/hk"
-    }
-]
+  {
+    id: 1,
+    image: "/svg/flag.svg",
+    title: "India",
+    port: "/analysis/india",
+  },
+  {
+    id: 2,
+    image: "/svg/flag.svg",
+    title: "US",
+    port: "/analysis/us",
+  },
+  {
+    id: 3,
+    image: "/svg/flag.svg",
+    title: "UK",
+    port: "/analysis/uk",
+  },
+  {
+    id: 4,
+    image: "/svg/flag.svg",
+    title: "Saudi",
+    port: "/analysis/saudi",
+  },
+  {
+    id: 5,
+    image: "/svg/flag.svg",
+    title: "Hong Kong",
+    port: "/analysis/hk",
+  },
+];
 
 export const intervalOptions = [
-    {
-      label: 'Minutes',
-      options: [
-        { value: '15 minutes', label: '15' },
-        { value: '30 minutes', label: '30' },
-        { value: '60 minutes', label: '60' }
-      ]
-    },
-    {
-      label: 'Other',
-      options: [
-        { value: 'daily', label: 'Daily' },
-        { value: 'weekly', label: 'Weekly' },
-        { value: 'monthly', label: 'Monthly' },
-        { value: 'quarterly', label: 'Quarterly' },
-        { value: 'half-yearly', label: 'Half-Yearly' },
-        { value: 'yearly', label: 'Yearly' }
-      ]
-    }
-  ];
+  {
+    label: "Minutes",
+    options: [
+      { value: "15 minutes", label: "15" },
+      { value: "30 minutes", label: "30" },
+      { value: "60 minutes", label: "60" },
+    ],
+  },
+  {
+    label: "Other",
+    options: [
+      { value: "daily", label: "Daily" },
+      { value: "weekly", label: "Weekly" },
+      { value: "monthly", label: "Monthly" },
+      { value: "quarterly", label: "Quarterly" },
+      { value: "half-yearly", label: "Half-Yearly" },
+      { value: "yearly", label: "Yearly" },
+    ],
+  },
+];
 export const intervalDerivativeOptions = [
-    {
-        label: 'Earnings',
-        options: [
-          { value: 'Earnings', label: 'Earnings' }
-        ]
-    },
-    {
-      label: 'Minutes',
-      options: [
-        { value: '15 minutes', label: '15' },
-        { value: '30 minutes', label: '30' },
-        { value: '60 minutes', label: '60' }
-      ]
-    },
-    {
-      label: 'Other',
-      options: [
-        { value: 'daily', label: 'Daily' },
-        { value: 'weekly', label: 'Weekly' },
-        { value: 'monthly', label: 'Monthly' },
-      ]
-    }
-  ];
-  
+  {
+    label: "Earnings",
+    options: [{ value: "Earnings", label: "Earnings" }],
+  },
+  {
+    label: "Minutes",
+    options: [
+      { value: "15 minutes", label: "15" },
+      { value: "30 minutes", label: "30" },
+      { value: "60 minutes", label: "60" },
+    ],
+  },
+  {
+    label: "Other",
+    options: [
+      { value: "daily", label: "Daily" },
+      { value: "weekly", label: "Weekly" },
+      { value: "monthly", label: "Monthly" },
+    ],
+  },
+];
 
 // export const LatestNewsData = [
 //     {
